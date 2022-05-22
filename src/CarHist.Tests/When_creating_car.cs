8d2e0d2f8c4f7986abed7feeaa7c361d8bcf6b6a
Georgi Shukov <georgi.shukov98@gmail.com>
2022-05-22 13:58:33 +0300

    Adds unit test for editing a car

--- src/CarHist.Tests/When_creating_car.cs
+++ src/CarHist.Tests/When_creating_car.cs
@@ -1,3 +1,4 @@
+using System;
 using CarHist.Cars;
 using CarHist.Cars.Events;
 using Elders.Cronus.Testing;
@@ -11,7 +12,7 @@ namespace CarHist.Tests
         static CarId id;
         static string make;
         static string model;
-        static string VIN;
+        static string vin;
         static string engineType;
         static Car car;
 
@@ -20,11 +21,11 @@ namespace CarHist.Tests
             id = new CarId("test", "pruvit");
             make = "BMW";
             model = "M3";
-            VIN = "123123";
+            vin = "123123";
             engineType = "petrol";
         };
 
-        Because of = () => car = new Car(id, make, model, VIN, engineType);
+        Because of = () => car = new Car(id, make, model, vin, engineType);
 
         It should_have_a_new_car = () => car.IsEventPublished<CarCreated>().ShouldBeTrue();
 
@@ -32,7 +33,50 @@ namespace CarHist.Tests
 
         It should_have_proper_model = () => car.RootState().Model.ShouldEqual(model);
 
-        It should_have_proper_VIN = () => car.RootState().VIN.ShouldEqual(VIN);
+        It should_have_proper_VIN = () => car.RootState().VIN.ShouldEqual(vin);
+
+        It should_have_proper_engineType = () => car.RootState().EngineType.ShouldEqual(engineType);
+
+        It should_have_proper_id = () => car.RootState().Id.ShouldEqual(id);
+    }
+
+    [Subject(nameof(Car))]
+    class When_edit_car
+    {
+        static CarId id;
+        static string make;
+        static string model;
+        static string vin;
+        static string engineType;
+        static Car car;
+
+        static string newMake;
+
+        Establish context = () =>
+        {
+            id = new CarId("test", "pruvit");
+            make = "BMW";
+            model = "M3";
+            vin = "123123";
+            engineType = "petrol";
+
+            car = Aggregate<Car>
+                .FromHistory(stream => stream
+                    .AddEvent(new CarCreated(id, make, model, vin, engineType, DateTimeOffset.UtcNow))
+                );
+
+            newMake = "VW";
+        };
+
+        Because of = () => car.EditCar(id, newMake, model, vin, engineType);
+
+        It should_have_a_new_event = () => car.IsEventPublished<CarEdited>().ShouldBeTrue();
+
+        It should_have_proper_make = () => car.RootState().Make.ShouldEqual(newMake);
+
+        It should_have_proper_model = () => car.RootState().Model.ShouldEqual(model);
+
+        It should_have_proper_VIN = () => car.RootState().VIN.ShouldEqual(vin);
 
         It should_have_proper_engineType = () => car.RootState().EngineType.ShouldEqual(engineType);
 
