e52074f317c1a9657f326a6399a67ad226470d1d
Georgi Shukov <georgi.shukov98@gmail.com>
2022-05-24 11:49:02 +0300

    Adds command and event for car history

---
+++ src/CarHist/Cars/Commands/AddHistory.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+using Elders.Cronus;
+
+namespace CarHist.Cars.Commands;
+
+[DataContract(Namespace = BC.CarHist, Name = "805999fa-d6ad-490e-8b24-f803a150fab6")]
+public class AddHistory : ICommand
+{
+    AddHistory() { }
+
+    public AddHistory(CarId id, string type, string description)
+    {
+        Id = id;
+        Type = type;
+        Description = description;
+    }
+
+    [DataMember(Order = 1)]
+    public CarId Id { get; private set; }
+
+    [DataMember(Order = 2)]
+    public string Type { get; private set; }
+
+    [DataMember(Order = 3)]
+    public string Description { get; private set; }
+}
---
+++ src/CarHist/Cars/Events/HistoryAdded.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization;
+using Elders.Cronus;
+
+namespace CarHist.Cars.Events;
+
+[DataContract(Namespace = BC.CarHist, Name = "10351a6b-7bf8-44e1-9abf-16d3fa3199bd")]
+public class HistoryAdded : IEvent
+{
+    HistoryAdded() { }
+
+    public HistoryAdded(CarId id, string type, string description, DateTimeOffset timestamp)
+    {
+        Id = id;
+        Type = type;
+        Description = description;
+        Timestamp = timestamp;
+    }
+
+    [DataMember(Order = 1)]
+    public CarId Id { get; private set; }
+
+    [DataMember(Order = 2)]
+    public string Type { get; private set; }
+
+    [DataMember(Order = 3)]
+    public string Description { get; private set; }
+
+    [DataMember(Order = 4)]
+    public DateTimeOffset Timestamp { get; private set; }
+}
