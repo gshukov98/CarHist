49a92491eba99dcd913191e713a3b41cbc10ea69
Georgi Shukov <georgi.shukov98@gmail.com>
2022-05-24 16:31:09 +0300

    Adds new UI

---
+++ src/CarHist.Blazor.UI/Pages/AddCar.razor.cs
@@ -0,0 +1,42 @@
+﻿using CarHist.Blazor.UI.Models;
+using CarHist.Cars;
+using CarHist.Cars.Commands;
+using Elders.Cronus;
+using Elders.Cronus.MessageProcessing;
+using Microsoft.AspNetCore.Components;
+
+namespace CarHist.Blazor.UI.Pages;
+
+public partial class AddCar : ComponentBase
+{
+    public CarStateUI car = new CarStateUI();
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    [Inject]
+    protected CronusContext CronusContext { get; set; }
+
+    [Inject]
+    protected IPublisher<ICommand> Publisher { get; set; }
+
+
+    string make;
+    string model;
+    string vin;
+    string engineType;
+
+    public void Insert()
+    {
+        //??
+        CarId id = new CarId(vin, CronusContext.Tenant);
+
+        //create car command
+        //add checks for values
+        var command = new CreateCar(id, make, model, vin, engineType);
+
+        Publisher.Publish(command);
+
+        NavigationManager.NavigateTo("/cars");
+    }
+}
---
+++ src/CarHist.Blazor.UI/Pages/Cars.razor.cs
@@ -0,0 +1,53 @@
+﻿using CarHist.Blazor.UI.Models;
+using CarHist.Blazor.UI.Services;
+using Elders.Cronus;
+using Elders.Cronus.MessageProcessing;
+using Elders.Cronus.Projections;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace CarHist.Blazor.UI.Pages;
+
+public partial class Cars : ComponentBase
+{
+    private HubConnection hubConnection;
+
+    [Inject]
+    protected IProjectionReader Projections { get; set; }
+
+    [Inject]
+    protected CarsProvider CarsProvider { get; set; }
+
+    [Inject]
+    protected CronusContext CronusContext { get; set; }
+
+    [Inject]
+    protected IPublisher<ICommand> Publisher { get; set; }
+
+    protected List<CarStateUI> cars = new List<CarStateUI>();
+
+    protected override async Task OnInitializedAsync()
+    {
+        cars = CarsProvider.GetCars().ToList();
+        hubConnection = new HubConnectionBuilder()
+            .WithUrl("http://127.0.0.1:17677" + "/hub/cars")
+            .WithAutomaticReconnect()
+            .Build();
+
+        hubConnection.On<string, string>("CarCreation", (carId, name) =>
+          {
+              if (cars.Any(x => x.VIN.Equals(carId) == false))
+                  cars.Add(new CarStateUI(carId, name));
+
+              StateHasChanged();
+          });
+
+        await hubConnection.StartAsync();
+    }
+
+    public void Dispose()
+    {
+        if (hubConnection is not null)
+            hubConnection.DisposeAsync();
+    }
+}
