1872b0ab763f2313e5d1a2dac656c87ee450c1bf
Georgi Shukov <georgi.shukov98@gmail.com>
2022-05-28 11:52:24 +0300

    Adds Index page

---
+++ src/CarHist.Blazor.UI/Pages/Index.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CarHist.Blazor.UI.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        private string searchVIN;
+
+        public void Insert()
+        {
+        }
+    }
+}
