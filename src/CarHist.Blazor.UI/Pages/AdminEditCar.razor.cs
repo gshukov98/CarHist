c8567a1262313ecd93449357626a448bbfa0db79
Georgi Shukov <georgi.shukov98@gmail.com>
2022-06-04 16:49:17 +0300

    Adds input validation of AdminEditCar

--- src/CarHist.Blazor.UI/Pages/AdminEditCar.razor.cs
+++ src/CarHist.Blazor.UI/Pages/AdminEditCar.razor.cs
@@ -5,6 +5,7 @@ using Elders.Cronus.MessageProcessing;
 using Microsoft.AspNetCore.Components;
 using CarHist.Cars;
 using CarHist.Cars.Commands;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarHist.Blazor.UI.Pages;
 
@@ -28,11 +29,15 @@ public partial class AdminEditCar : ComponentBase
     [Inject]
     protected IPublisher<ICommand> Publisher { get; set; }
 
+    private EditCarInputModel EditCarInputModel;
+
     protected override Task OnInitializedAsync()
     {
         if (string.IsNullOrEmpty(Id) == false)
         {
             car = CarsProvider.GetCars().Where(x => x.VIN.Equals(Id)).FirstOrDefault();
+
+            EditCarInputModel = new EditCarInputModel(car.Make, car.Model, car.VIN, car.EngineType);
         }
 
         return base.OnInitializedAsync();
@@ -40,12 +45,12 @@ public partial class AdminEditCar : ComponentBase
 
     public void Update()
     {
-        if (string.IsNullOrEmpty(car.VIN))
+        if (string.IsNullOrEmpty(EditCarInputModel.VIN))
             return;
 
-        CarId carId = CarId.Parse(FormatCarId(car.VIN));
+        CarId carId = CarId.Parse(FormatCarId(EditCarInputModel.VIN));
 
-        var command = new EditCar(carId, car.Make, car.Model, car.VIN, car.EngineType);
+        var command = new EditCar(carId, EditCarInputModel.Make, EditCarInputModel.Model, EditCarInputModel.VIN, EditCarInputModel.EngineType);
         Publisher.Publish(command);
 
         #region Don't look inside
@@ -57,3 +62,35 @@ public partial class AdminEditCar : ComponentBase
 
     private string FormatCarId(string id) => $"urn:{CronusContext.Tenant}:car:{id}";
 }
+
+public class EditCarInputModel
+{
+    public EditCarInputModel()
+    {
+
+    }
+
+    public EditCarInputModel(string make, string model, string VIN, string engineType)
+    {
+        Make = make;
+        Model = model;
+        this.VIN = VIN;
+        EngineType = engineType;
+    }
+
+    [Required]
+    [MinLength(2), MaxLength(20)]
+    public string Make { get; set; }
+
+    [Required]
+    [MinLength(2), MaxLength(25)]
+    public string Model { get; set; }
+
+    [Required]
+    public string VIN { get; set; }
+
+    [Required]
+    [MinLength(6), MaxLength(8)]
+    public string EngineType { get; set; }
+}
+
