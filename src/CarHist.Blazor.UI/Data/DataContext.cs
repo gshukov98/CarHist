cfe06cd7d455eca96f5433e5e8685f2535880190
Georgi Shukov <georgi.shukov98@gmail.com>
2022-06-06 20:40:44 +0300

    Adds base setup

---
+++ src/CarHist.Blazor.UI/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarHist.Blazor.UI.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
